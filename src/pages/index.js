6b2136385e5d5bc2921220bce206c984bdd8cc97
codeanish <anish@codeanish.com>
2020-10-15 17:48:44 +0100

    Adding separate pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
 import React from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        <li><a href="/blog">Blog</a></li>
+        <li><a href="/about">About</a></li>
+      </ul>
+    </div>
+  )
 }
+
+export default Home;
