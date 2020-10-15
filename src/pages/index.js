a8544e3109cb47dd2334f19876414322180668ba
codeanish <anish@codeanish.com>
2020-10-15 18:07:13 +0100

    Adding gatsby Link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
+import {Link} from 'gatsby'
 
 const Home = () => {
   return (
     <div>
       <h1>Home</h1>
       <ul>
-        <li><a href="/blog">Blog</a></li>
-        <li><a href="/about">About</a></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/about">About</Link></li>
       </ul>
     </div>
   )
