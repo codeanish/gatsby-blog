d6b1bc73ad05d83840e5ea2ea1862f7efdec560f
Anish <anish@codeanish.com>
2020-10-29 21:34:42 +0000

    Defining custom renderers

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout'
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer'
+import {BLOCKS} from '@contentful/rich-text-types'
 
 const BlogPost = ({pageContext}) => {     
     const {blogPost} = pageContext
     
+    const options = {
+        renderNode:{
+            [BLOCKS.EMBEDDED_ASSET]: (node) => {
+                const url = node.data.target.fields.file['en-US'].url
+                const alt = node.data.target.fields.title['en-US']
+                return <img src={url} alt={alt}/>
+            }
+        }
+    }
+
     return(
         <Layout>
             <h1>{blogPost.title}</h1>
             <h2>{blogPost.publishedDate}</h2>
-            {documentToReactComponents(blogPost.body.json)}
+            {documentToReactComponents(blogPost.body.json, options)}
         </Layout>
     )
 
